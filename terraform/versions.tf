89f2d5293166894682b58530c866282b5a62316f
Joao Guilherme <joaoguilhermesilvasouza@gmail.com>
2025-12-11 22:03:33 -0300

    resolvendo duplicação do terraform

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,18 +1,10 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  backend "remote" {
-    organization = "ProjetoDocker"
-
-    workspaces {
-      name = "projeto-docker"
-    }
-  }
-
   required_providers {
     digitalocean = {
       source  = "digitalocean/digitalocean"
-      version = "~> 2.0"
+      version = "~> 2.22"
     }
   }
 }
